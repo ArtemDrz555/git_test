776031ad248e58df506e5c66a9b438384f2d791e
Artem D <ad6840236@gmail.com>
2024-08-25 22:19:06 +0600

    mister master

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int* PApointer;
+    int* pScore = 0;
+    int score = 1000;
+    pScore = &score;
+    
+    return 0;
+}
